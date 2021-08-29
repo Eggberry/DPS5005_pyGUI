5a7bb876f0d5d75a906a237b7134b01f9b5a8cc9
Eggberry <Eggberry@users.noreply.github.com>
2021-08-29 17:51:58 +0200

    Add files via upload

---
+++ source_files/dp6012_register_test.py
@@ -0,0 +1,24 @@
+ 
+import minimalmodbus
+ 
+ 
+instrument = minimalmodbus.Instrument('/dev/ttyUSB1', 1) # port name, slave address (in decimal)
+instrument.serial.port          # this is the serial port name
+instrument.serial.baudrate = 115200   # Baud rate used. see setting in DP unit
+instrument.serial.bytesize = 8
+instrument.serial.timeout = 0.5     # common used
+ 
+ 
+instrument.mode = minimalmodbus.MODE_RTU  #RTU mode 
+ 
+
+#instrument.write_register(8,11)
+#instrument.write_register(9,300)
+
+dump = instrument.read_registers(0x00,20)
+print (dump)
+
+# for i in range(129):
+#     output = instrument.read_register(i, 0)   #read registers 0x0 to 0x80
+#     #print(hex(i)[2:].zfill(2), output)
+#     print (output)
